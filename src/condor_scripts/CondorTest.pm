59670bf1c83f95a72eb68d56d23d309c4c5bc987
Greg Thain <gthain@cs.wisc.edu>
2025-07-27 18:58:54 -0500

    Fixup test problem when no domainname

--- src/condor_scripts/CondorTest.pm
+++ src/condor_scripts/CondorTest.pm
@@ -2160,7 +2160,14 @@ sub spawn_cmd
 
 sub getFqdnHost {
     my $host = hostfqdn();
-    CondorUtils::fullchomp($host);
+    CondorUtils::fullchomp($host)
+    # Fixup hostnames ending in two dots when no domain is set
+    if ($host =~ /\.$/) {
+      chop($host);
+    }
+    if ($host =~ /\.$/) {
+      chop($host);
+    }
     return($host);
 }
 
