a80693f8cb20e6d646c74baea71b8e729399e1e7
Greg Thain <gthain@cs.wisc.edu>
2025-12-31 19:02:44 -0600

    Add echo=True to run_command

--- src/condor_tests/test_x509_delegation.py
+++ src/condor_tests/test_x509_delegation.py
@@ -27,6 +27,7 @@ def path_to_the_x509_cred(default_condor, test_dir):
     rv = default_condor.run_command(
         ["openssl", "req", "-nodes", "-x509", "-newkey", "rsa:4096", "-keyout", key_path.as_posix(), "-out", cred_path.as_posix(), "-days", "365", "-subj", "/C=US/ST=Wisconsin/L=Madison/O=HTCondor/CN=Test Credential"],
         timeout=8,
+        echo=True,
     )
     assert rv.returncode == 0
     key_data = key_path.read_text()
