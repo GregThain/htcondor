44bc92132f6d4b72d97dec93872a82e2c6240c53
Greg Thain <gthain@cs.wisc.edu>
2025-12-31 19:28:16 -0600

    90 second timeout for openssl req

--- src/condor_tests/test_x509_delegation.py
+++ src/condor_tests/test_x509_delegation.py
@@ -26,7 +26,7 @@ def path_to_the_x509_cred(default_condor, test_dir):
     cred_path = test_dir / "test.pem"
     rv = default_condor.run_command(
         ["openssl", "req", "-nodes", "-x509", "-newkey", "rsa:4096", "-keyout", key_path.as_posix(), "-out", cred_path.as_posix(), "-days", "365", "-subj", "/C=US/ST=Wisconsin/L=Madison/O=HTCondor/CN=Test Credential"],
-        timeout=8,
+        timeout=90,
         echo=True,
     )
     assert rv.returncode == 0
