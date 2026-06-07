2df2bebaac40d62770fdc70852db3c5e35943fba
Greg Thain <gthain@cs.wisc.edu>
2026-06-07 18:07:38 -0500

    Clense

--- src/condor_tests/ctest_driver.py
+++ src/condor_tests/ctest_driver.py
@@ -180,11 +180,6 @@ def main():
         os.environ["PATH"] += ";" + os.path.join(args.prefix_path, "..\\msconfig")
         os.environ["PATH"] += ";" + os.path.join(args.prefix_path, "bin")
         os.environ["PATH"] += ";" + os.path.join(args.prefix_path, "..\\src\\condor_tests")
-        print("GGT\n")
-        print(os.environ["PATH"])
-        print("GGT running condor_who\n")
-        os.system("condor_who")
-        print("GGT done running condor_who\n")
     else:
         os.environ["PATH"] = os.pathsep.join(
             [
