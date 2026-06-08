9b3c5eada12cfc2fc11b0afee116c4290dadea14
Greg Thain <gthain@cs.wisc.edu>
2026-06-07 20:25:14 -0500

    comment out run commit

--- src/condor_scripts/run_test.pl
+++ src/condor_scripts/run_test.pl
@@ -486,8 +486,6 @@ sub DoChild
 
     my $res;
     my $use_timed_cmd = 1; # use the timed_cmd helper binary to timeout the test and cleaup processes
-	print("GGT GGT GGT no use timed cmd\n");
-	$use_timed_cmd = 0;
     if ($iswindows && $use_timed_cmd) {
         my $dtm = ""; if (defined $ENV{TIMED_CMD_DEBUG_WAIT}) {$dtm = ":$ENV{TIMED_CMD_DEBUG_WAIT}";}
         my $verb = ($hush == 0) ? "" : "-v";
